3c5e413fe0375e1e09bd20b855538bab51cefcd7
eugene tan <eulataguhw@gmail.com>
2026-01-17 17:14:26 +0800

    cleaning up

--- frontend/src/i18n/i18n.ts
+++ frontend/src/i18n/i18n.ts
@@ -1,14 +1,14 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import enJSON from './locales/en.json';
+import * as i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import enJSON from "./locales/en.json";
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     debug: true,
-    fallbackLng: 'en',
+    fallbackLng: "en",
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
